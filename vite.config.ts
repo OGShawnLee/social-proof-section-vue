eaedc7153aaf113c5ae7ca1d40031c66cd5bb4c1
Shawn Lee <changoloco.evg@gmail.com>
2022-05-02 13:16:31 -0500

    :wrench: [chore]: configure unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import unocss from 'unocss/vite';
-import { presetAttributify, presetUno } from 'unocss';
+import { presetAttributify, presetUno, transformerVariantGroup } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,22 @@ export default defineConfig({
     vue(),
     unocss({
       presets: [presetAttributify(), presetUno()],
+      theme: {
+        colors: {
+          black: 'hsl(0, 0, 0)',
+          magenta: {
+            100: 'hsl(300, 24%, 96%)',
+            300: 'hsl(303, 10%, 53%)',
+            700: 'hsl(300, 43%, 22%)',
+          },
+          pink: 'hsl(333, 80%, 67%)',
+          white: 'hsl(0, 0%, 100%)',
+        },
+        fontFamily: {
+          spartan: ['League Spartan', 'sans-serif'],
+        },
+      },
+      transformers: [transformerVariantGroup()],
     }),
   ],
 });
