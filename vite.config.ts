d5680b4117bd277970da2799695f1fd2d39a61d3
Shawn Lee <changoloco.evg@gmail.com>
2022-05-02 12:05:26 -0500

    :heavy_plus_sign: [chore](deps): add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import unocss from 'unocss/vite';
+import { presetAttributify, presetUno } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    unocss({
+      presets: [presetAttributify(), presetUno()],
+    }),
+  ],
 });
